889802b6ad75b0a46201c2096a2396d8e8ad5a16
LaithAlamat <laithalamat4@gmail.com>
2022-05-09 21:10:26 +0300

    lab18 done!

--- Async Inn/Async Inn/Models/Interfaces/IUserService.cs
+++ Async Inn/Async Inn/Models/Interfaces/IUserService.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Async_Inn.Models.Interfaces
 {
-    interface IUserService
+    public interface IUserService
     {
         public Task<UserDTO> Register(RegisterUserDTO data, ModelStateDictionary modelState);
         public Task<UserDTO> Authenticate(string username, string password);
--- Async Inn/Async Inn/Models/Interfaces/Services/IdentityUserRepository.cs
+++ Async Inn/Async Inn/Models/Interfaces/Services/IdentityUserRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Async_Inn.Models.DTOs;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.Collections.Generic;
@@ -9,14 +10,64 @@ namespace Async_Inn.Models.Interfaces.Services
 {
     public class IdentityUserRepository : IUserService
     {
-        public Task<UserDTO> Authenticate(string username, string password)
+        private UserManager<ApplicationUser> _userManager;
+        public IdentityUserRepository(UserManager<ApplicationUser> userManager)
         {
-            throw new NotImplementedException();
+            _userManager = userManager;
+        }
+        
+        public async Task<UserDTO> Authenticate(string username, string password)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+            if (user != null)
+            {
+                if (await _userManager.CheckPasswordAsync(user, password))
+                {
+                    UserDTO userDto = new UserDTO
+                    {
+                        Id = user.Id,
+                        Username = user.UserName,
+                    };
+                    return userDto;
+                }
+            }
+            return null;
         }
 
-        public Task<UserDTO> Register(RegisterUserDTO data, ModelStateDictionary modelState)
+        public async Task<UserDTO> Register(RegisterUserDTO data, ModelStateDictionary modelState)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            var user = new ApplicationUser
+            {
+                UserName = data.Username,
+                Email = data.Email,
+                PhoneNumber = data.PhoneNumber
+            };
+
+            var result = await _userManager.CreateAsync(user, data.Password);
+            if (result.Succeeded)
+            {
+                UserDTO userDto = new UserDTO
+                {
+                    Id = user.Id,
+                    Username = user.UserName,
+                };
+                return userDto;
+            }
+            foreach (var error in result.Errors)
+            {
+                var errorKey =
+                  error.Code.Contains("Password") ? nameof(data.Password) :
+                  error.Code.Contains("Email") ? nameof(data.Email) :
+                  error.Code.Contains("UserName") ? nameof(data.Username) :
+                  "";
+
+                modelState.AddModelError(errorKey, error.Description);
+            }
+
+            return null;
         }
+
+       
     }
 }
